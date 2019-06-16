5f53a22882c3fedc409bdb46cd6b6be66625156a
Yijia Chen <jyc.chen@mail.utoronto.ca>
2019-06-16 17:15:32 -0400

    Finish untested first pass

--- im_binning.py
+++ im_binning.py
@@ -74,11 +74,14 @@ def run_ff(exp, type):
     features.setUniqueIds()
     return features
 
-def find_features(spec):
+def find_features(spec, outdir, outfile, spec_idx=0):
     """Make one pass at binning a spectrum and finding its features.
 
     Args:
         spec (MSSpectrum): An OpenMS MSSpectrum object.
+        outdir (string): The output directory for FeatureXML files.
+        outfile (string): A string to identify this series of runs.
+        spec_idx (int): The index of this spectrum in a series of spectra.
     """
     points = get_points(spec)
     # Sort points by IM ascending
@@ -86,11 +89,14 @@ def find_features(spec):
 
     # Position of bin i (0-indexed) = i * bin_size + first_im
     first_im, last_im = sorted_points[0][3], sorted_points[len(points) - 1][3]
-    delta_im = last_im - first_im
+    delta_im, offset_im = last_im - first_im, 0
     
     num_bins = 50
     bin_size = delta_im / num_bins
     bins = [[]] * num_bins
+
+    # For successive passes, use offset_im to shift bins
+    offset_delta, pass_num = 0.05, 1
 
     new_exp = []
     for i in range(num_bins):
@@ -98,16 +104,58 @@ def find_features(spec):
 
     # Step 1: assign points to bins
     for i in range(len(points)):
+        # Need to adapt this formula for offset_im
         bin_idx = int((sorted_points[i][3] - first_im) / delta_im)
         bins[bin_idx].append(sorted_points[i])
 
     # Step 2: for each m/z, average the intensities
     for i in range(num_bins):
-        pass
+        bins[i] = sorted(bins[i], key=itemgetter(1))
+        mz_start, num_mz, curr_mz = 0, 0, bins[i][0][1]
+        run_intensity = 0
+
+        for j in range(len(bins[i])):
+            if (bins[i][j][1] == curr_mz):
+                num_mz += 1
+                run_intensity += bins[i][j][2]
+            else:
+                # Reached a new m/z slice; update the previous intensities
+                run_intensity /= num_mz
+                for k in range(mz_start, mz_start + num_mz):
+                    bins[i][k][2] = run_intensity
+
+                mz_start, num_mz, curr_mz = j, 0, bins[i][j][1]
+                run_intensity = 0
+
+        # Takes care of the last slice (if required)
+        if num_mz > 0:
+            run_intensity /= num_mz
+            for k in range(mz_start, mz_start + num_mz):
+                bins[i][k][2] = run_intensity
+
+        # Get the arrays of RT, MZ, Intensity, and IM
+        transpose = list(zip(*bins[i]))
+
+        new_spec = ms.MSSpectrum()
+        new_spec.setRT(spec.getRT())
+        new_spec.setPeaks((list(transpose[1]), list(transpose[2])))
+
+        fda = ms.FloatDataArray()
+        for j in list(transpose[3]):
+            fda.push_back(j)
+        new_spec.setFloatDataArrays([fda])
+
+        new_exp[i].addSpectrum(new_spec)
+        ms.MzMLFile().store(outdir + '/' + outfile + '-spec' + str(spec_idx) + '-pass' +
+                            str(pass_num) + '-bin' + str(i) + '.mzML', new_exp[i])
 
     # Step 3: find the features for each bin
+    ff_type = 'centroided'
     for i in range(num_bins):
-        pass
+        features = run_ff(new_exp[i], ff_type)
+        ms.FeatureXMLFile().store(outdir + '/' + outfile + '-spec' + str(spec_idx) +
+                                  '-pass' + str(pass_num) + '-bin' + str(i) +
+                                  '.featureXML', features)
 
 if __name__ == "__main__":
     # Includes legacy arguments from baseline.py
@@ -129,4 +177,4 @@ if __name__ == "__main__":
     spectra = exp.getSpectra()
     for i in range(args.num_frames):
         spec = spectra[i]
-        find_features(spec)
+        find_features(spec, args.outdir, args.outfile, i)
