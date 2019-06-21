e6e1479c8a28b7d4ef8aff16323a246253e532bf
Yijia Chen <jyc.chen@mail.utoronto.ca>
2019-06-21 14:28:00 -0400

    Create feature comparison tool

---
+++ cmp_features.py
@@ -0,0 +1,53 @@
+import argparse
+
+import pyopenms as ms
+import numpy as np
+
+import time
+from operator import itemgetter
+
+from im_binning import run_ff
+
+def get_f_points(features):
+    arr_rt, arr_mz, arr_intensity = [], [], []
+    for feature in features:
+        arr_rt.append(feature.getRT())
+        arr_mz.append(feature.getMZ())
+        arr_intensity.append(feature.getIntensity())
+
+    return [arr_rt, arr_mz, arr_intensity]
+
+def compare_features(found_features_map, openms_features_map):
+    f1 = get_f_points(found_features_map)
+    f2 = get_f_points(openms_features_map)
+
+    found_features = sorted(f1, key=itemgetter(0, 1, 2))
+    openms_features = sorted(f2, key=itemgetter(0, 1, 2))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Feature comparison.')
+    parser.add_argument('--found', action='store', required=True, type=str)
+    parser.add_argument('--openms', action='store', required=False, type=str)
+    parser.add_argument('--source', action='store', required=False, type=str)
+    parser.add_argument('--out', action='store', required=True, type=str)
+
+    args = parser.parse_args()
+    found_features, openms_features = ms.FeatureMap(), ms.FeatureMap()
+
+    if args.openms is None:
+        if args.source is None:
+            print("Error: a source mzML file is required for this argument set.")
+            quit()
+
+        exp = ms.MSExperiment()
+        print('Loading mzML input file....................', end='', flush=True)
+        ms.MzMLFile().load(args.source + '.mzML', exp)
+        print('Done')
+
+        openms_features = run_ff(exp, 'centroided')
+    else:
+        ms.FeatureXMLFile.load(args.openms + '.mzML', openms_features)
+
+    ms.FeatureXMLFile.load(args.found + '.mzML', found_features)
+    
+    compare_features(found_features, openms_features)
