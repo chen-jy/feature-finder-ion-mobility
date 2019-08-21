06569c50c9227553bf12c25609fab21ee9a76a95
Yijia Chen <jyc.chen@mail.utoronto.ca>
2019-08-21 11:48:42 -0400

    Restructure file directory

---
+++ utils/extract_ms1.py
@@ -0,0 +1,24 @@
+import argparse
+import pyopenms as ms
+import numpy as np
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Extract MS1 spectra from an mzML file.')
+    parser.add_argument('--input', action='store', required=True, type=str)
+    parser.add_argument('--output', action='store', required=True, type=str)
+
+    args = parser.parse_args()
+
+    exp = ms.MSExperiment()
+    ms.MzMLFile().load(args.input, exp)
+
+    spectra = exp.getSpectra()
+    new_spectra = []
+
+    for spec in spectra:
+        if spec.getMSLevel() == 1:
+             new_spectra.append(spec)
+
+    exp.setSpectra(new_spectra)
+    ms.MzMLFile().store(args.output, exp)
+
