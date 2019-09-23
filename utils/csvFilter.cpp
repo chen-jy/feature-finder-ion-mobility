dd3ace8d46b5a07683d907311a312e18c0a5c0ff
Yijia Chen <jyc.chen@mail.utoronto.ca>
2019-09-23 12:36:18 -0400

    Fix csv filtering

--- utils/csvFilter.cpp
+++ utils/csvFilter.cpp
@@ -13,6 +13,7 @@
 #endif
 
 #define Q_THRESHOLD 0.01
+#define PEAK_GROUP_RANK 1
 
 using namespace std;
 
@@ -46,7 +47,7 @@ int main(int argc, char *argv[]) {
 			if (mode < 0 || mode > 2) {
 				cout << "Error: invalid mode\n";
 				cout << "       <mode> must be 0 (q filtering), 1 (rt/mz/int projection), or 2 (int sorting)\n";
-				exit(2);
+				exit(1);
 			}
 			break;
 		case 'a':
@@ -61,6 +62,12 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
+	// TODO: make -1 mean the entire RT range
+	if (mode == 0 && (min_rt == -1 || max_rt == -1)) {
+		cout << "Error: filtering requires RT bounds\n";
+		exit(1);
+	}
+
 	fstream infile(input_filename.c_str(), ios::in);
 	if (!infile.is_open()) {
 		cout << "Error: unable to open input csv file\n";
@@ -71,11 +78,6 @@ int main(int argc, char *argv[]) {
 	if (!outfile.is_open()) {
 		cout << "Error: unable to open output csv file\n";
 		exit(2);
-	}
-
-	if (mode == 0 && (min_rt == -1 || max_rt == -1)) {
-		cout << "Error: filtering requires RT bounds\n";
-		exit(3);
 	}
 
 	string input;
@@ -94,7 +96,6 @@ int main(int argc, char *argv[]) {
 
 	// Projection only requires these three columns
 	if (mode == 1) {
-		// aggr_Peak_Area for the precursor intensity?
 		vector<string> header{ "RT", "m/z", "Intensity" };
 		table.push_back(header);
 	}
@@ -114,15 +115,16 @@ int main(int argc, char *argv[]) {
 			// Use initialPeakQuality on non-scored tsv files
 			double q = stod(line[headers.find("q_value")->second]);
 			double d = stoi(line[headers.find("decoy")->second]);
+			int pgr = stoi(line[headers.find("peak_group_rank")->second]);
 
-			if (rt >= min_rt && rt <= max_rt && q >= Q_THRESHOLD && !d) {
+			if (rt >= min_rt && rt <= max_rt && q <= Q_THRESHOLD && !d && pgr == PEAK_GROUP_RANK) {
 				table.push_back(line);
 			}
 		}
 		else if (mode == 1) {
 			string rt = line[headers.find("RT")->second];
 			string mz = line[headers.find("m/z")->second];
-			string intensity = line[headers.find("Intensity")->second];
+			string intensity = line[headers.find("aggr_prec_Peak_Area")->second];
 
 			vector<string> projection{ rt, mz, intensity };
 			table.push_back(projection);
@@ -144,7 +146,7 @@ int main(int argc, char *argv[]) {
 	if (mode == 2) {
 		vector<pair<double, vector<string> *>> sorted;
 		for (int i = 1; i < table.size(); i++) {
-			double intensity = stod(table[i][headers.find("Intensity")->second]);
+			double intensity = stod(table[i][headers.find("aggr_prec_Peak_Area")->second]);
 			sorted.push_back({ intensity, &table[i] });
 		}
 
