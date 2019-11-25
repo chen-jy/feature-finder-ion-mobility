809a231cc7d4143f103a0fddbf4e507bc092338e
Yijia Chen <jyc.chen@mail.utoronto.ca>
2019-11-24 22:24:14 -0500

    Fix csv cleaning tool

--- utils/dda_cleaner.py
+++ utils/dda_cleaner.py
@@ -12,29 +12,42 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Projection')
     parser.add_argument('--input', action='store', required=True, type=str)
     parser.add_argument('--output', action='store', required=True, type=str)
+    parser.add_argument('--file', action='store', required=True, type=str)
     parser.add_argument('--start', action='store', required=True, type=float)
     parser.add_argument('--stop', action='store', required=True, type=float)
     args = parser.parse_args()
     
     data = []
+    errorFalse, errorSmall = 0, 0
+    cleanRows = 0
     
-    with open(args.input, newline='') as csvfile:
+    with open(args.input, 'r', newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
+            rf = row['Raw file']
             rt = checkFloat(row['Retention time'])
             mz = checkFloat(row['m/z'])
             intensity = checkFloat(row['Intensity'])
             
+            if rf != args.file:
+                continue
             if rt is False or mz is False or intensity is False:
+                errorFalse += 1
                 continue
             if mz <= 10 or intensity <= 10:
+                errorSmall += 1
                 continue
             if args.start <= rt <= args.stop:
                 data.append([rt, mz, intensity])
+                cleanRows += 1
                 
     data = sorted(data, key=itemgetter(2))
             
     with open(args.output, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerows(data)
+    
+    print("'False' errors:", errorFalse)
+    print("'Small' errors:", errorSmall)
+    print("Total rows:", cleanRows)
     
