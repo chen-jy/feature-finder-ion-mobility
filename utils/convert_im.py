243883ea429a133b66e6cc0cd91ade0f0e7d4bd9
Yijia Chen <jyc.chen@mail.utoronto.ca>
2020-02-09 23:50:18 -0500

    Add raw data ook0 conversion script

---
+++ utils/convert_im.py
@@ -0,0 +1,27 @@
+import csv
+import pyopenms as ms
+import numpy as np
+import diapysef.timsdata as tims
+
+td = tims.TimsData('raw-files/20180819_TIMS2_12-2_AnBr_SA_200ng_HeLa_50cm_120min_100ms_11CT_2_A1_01_2768.d')
+conn = td.conn
+
+q = conn.execute('SELECT COUNT(*) FROM Frames')
+row = q.fetchone()
+N = row[0]
+
+scan_number_axis = np.arange(N, dtype=np.float64)
+ook0_axis = td.scanNumToOneOverK0(1, scan_number_axis)
+
+lines = []
+with open('csv-files/allPeptides-projected.csv', 'r') as file:
+    reader = csv.reader(file)
+    for line in reader:
+        lines.append(line)
+
+for i in range(len(lines)):
+    lines[i][2] = ook0_axis[int(lines[i][2])]
+
+with open('csv-files/allPeptides-projected-im.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(lines)
