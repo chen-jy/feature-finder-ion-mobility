9a5aa59726ee438e707a762e68b127698adce966
Yijia Chen <jyc.chen@mail.utoronto.ca>
2020-04-01 01:30:46 -0400

    Optimize comparison for runtime

--- compare_features.py
+++ compare_features.py
@@ -3,7 +3,6 @@
 
 import argparse
 import csv
-from math import floor
 from operator import itemgetter
 from typing import Any, List
 
@@ -13,23 +12,18 @@ import common_utils_im as util
 
 
 # For file writing
-output_group = ''
-multiple = None
+output_file = ''
 
 # Global statistics
-num_common = 0
-times_matched = [0, 0, 0, 0]  # Zero matches, one match, multiple matches
+times_matched = [0, 0, 0]  # Zero matches, one match, multiple matches
 
 # Program parameters
-rt_threshold = 0.0
-mz_threshold = 0.0
-im_threshold = 0.0
+thresholds = []
 
 
 def reset_stats() -> None:
     """Resets the global variables."""
-    global num_common, times_matched
-    num_common = 0
+    global times_matched
     times_matched = [0, 0, 0, 0]
 
 
@@ -62,8 +56,9 @@ def reset_csv_list(csv_list: List[List[float]]) -> None:
 
 def print_summary() -> None:
     """Prints and logs a summary of the most recently run comparison."""
-    global output_group, num_common, times_matched
-    with open(output_group + '-summary.txt', 'w') as f:
+    global output_file, times_matched
+    with open(output_file, 'w') as f:
+        num_common = times_matched[1] + times_matched[2]
         print('Common features:', num_common)
         f.write('Common features: %d\n' % num_common)
         print('No matches:', times_matched[0])
@@ -72,8 +67,6 @@ def print_summary() -> None:
         f.write('One match: %d\n' % times_matched[1])
         print('Multiple matches:', times_matched[2])
         f.write('Multiple matches: %d\n' % times_matched[2])
-        print('Missed:', times_matched[3])
-        f.write('Missed: %d\n' % times_matched[3])
 
 
 def convert_to_bidx(im: float) -> int:
@@ -88,46 +81,55 @@ def convert_to_bidx(im: float) -> int:
     return idx if idx < num_bins else num_bins - 1
 
 
-def compare(features1: list, features2: list) -> None:
-    """Compares a list of reference features against a list of found features, checking how many
-    times each reference feature maps to found features.
+def compare_features(features1: Any, features2: list, im_mode: bool = True, quiet: bool = True) -> None:
+    """Compares reference features against a list of found features, checking how many times each
+    reference feature maps to found features.
 
     The resulting statistics are written to the global statistics values for printing.
 
     Keyword arguments:
-    features1: the list of found features (e.g. by feature_finder_im)
+    features1: the found features (e.g. by feature_finder_im)
     features2: the list of reference features (e.g. converted from MaxQuant output)
+    im_mode: if true, compare by IM data in addition to RT and m/z data
+    quiet: if true, suppress output for current progress
     """
-    global output_group, num_common, times_matched, rt_threshold, mz_threshold, im_threshold
+    global times_matched, thresholds
     reset_stats()
     reset_csv_list(features2)
-    features2 = sorted(features2, key=itemgetter(2))  # Ascending IM
+    if type(features1) == list:
+        features1 = sorted(features1, key=itemgetter(0))
+    else:
+        features1.sortByRT()
 
     for j in range(len(features2)):
-        similar = []
+        if not quiet and j % 100 == 0:
+            print('Comparing feature', j + 1, 'of', len(features2))
 
-        attempted = False
-        for i in range(len(features1)):
-            if util.similar_features_im(features1[i], features2[j], rt_threshold, mz_threshold, im_threshold):
-                similar.append(features1[i])
-            elif util.similar_features(features1[i], features2[j]):
-                attempted = True
+        num_common = 0
 
-        if len(similar) == 0:
+        curr_rt = features2[j][0] - thresholds[0]
+        curr_idx = util.binary_search_left_rt(features1, curr_rt)
+        curr_rt = features1[curr_idx][0] if type(features1) == list else features1[curr_idx].getRT()
+
+        while curr_rt <= features2[j][0] + thresholds[0]:
+            if (im_mode and util.similar_features_im(features1[curr_idx], features2[j], *thresholds)) or \
+                (not im_mode and util.similar_features(features1[curr_idx], features2[j], thresholds[0],
+                                                       thresholds[1])):
+                num_common += 1
+                if num_common > 1:
+                    break
+
+            curr_idx += 1
+            if curr_idx == (len(features1) if type(features1) == list else features1.size()):
+                break
+            curr_rt = features1[curr_idx][0] if type(features1) == list else features1[curr_idx].getRT()
+
+        if num_common == 0:
             times_matched[0] += 1
-            if attempted:
-                times_matched[3] += 1
-        elif len(similar) == 1:
-            num_common += 1
+        elif num_common == 1:
             times_matched[1] += 1
         else:
-            num_common += 1
             times_matched[2] += 1
-
-            multiple.write('{:.6f}, {:.6f}, {:.6f}\n'.format(*features2[j]))
-            for feature in similar:
-                multiple.write('{:.6f}, {:.6f}, {:.6f}\n'.format(*feature))
-            multiple.write('==================================================\n')
 
     print_summary()
 
@@ -140,23 +142,32 @@ if __name__ == '__main__':
                         help='the reference features (e.g. found by MaxQuant)')
     parser.add_argument('-o', '--out', action='store', required=True, type=str,
                         help='the output group name (not a single filename)')
+
     parser.add_argument('-t', '--rt', action='store', required=False, type=float, default=5.0,
                         help='the RT threshold to use')
-    parser.add_argument('-m', '--mz', action='store', required=False, type=float, default=0.1,
+    parser.add_argument('-m', '--mz', action='store', required=False, type=float, default=0.01,
                         help='the m/z threshold to use')
     parser.add_argument('-z', '--im', action='store', required=False, type=float, default=0.031,
                         help='the IM threshold to use')
 
+    parser.add_argument('--no-im', action='store_true', required=False, default=False,
+                        help='do not use IM comparisons (only use RT and m/z)')
+    parser.add_argument('-q', '--quiet', action='store_true', required=False, default=False,
+                        help='suppress progress output when comparing features')
+
     args = parser.parse_args()
-    output_group = args.out
-    rt_threshold, mz_threshold, im_threshold = args.rt, args.mz, args.im
+    output_file = args.out
+    thresholds = [args.rt, args.mz, args.im]
 
     input_mask, ref_mask = ms.FeatureMap(), ms.FeatureMap()
     input_is_csv = True if args.in_.endswith('.csv') else False
     ref_is_csv = True if args.ref.endswith('.csv') else False
 
-    if not input_is_csv or not ref_is_csv:
-        print('Error: compare_features currently only supports csv-csv comparisons')
+    if not input_is_csv and not args.in_.endswith('featureXML'):
+        print('Error: input features must be in csv or featureXML formats')
+        exit(1)
+    if not ref_is_csv:
+        print('Error: comparison currently requires reference features in csv format')
         exit(1)
 
     if input_is_csv: input_mask = csv_to_list(args.in_)
@@ -165,9 +176,4 @@ if __name__ == '__main__':
     if ref_is_csv: ref_mask = csv_to_list(args.ref)
     else: ms.FeatureXMLFile().load(args.ref, ref_mask)
 
-    multiple = open(output_group + '-multiple.txt', 'w')
-
-    compare(input_mask, ref_mask)
-    #compare(ref_mask, input_mask)
-
-    multiple.close()
+    compare_features(input_mask, ref_mask, not args.no_im, args.quiet)
